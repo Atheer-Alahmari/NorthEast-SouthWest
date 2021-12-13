fe5869f1bc75c1a927ec6d36115845dd1d1e7caf
Atheer-Alahmari <Atheer.a.alahmari@gmail.com>
2021-12-13 15:55:26 +0300

    Initial Commit

---
+++ NorthEastـSouthWest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NorthEastـSouthWest
+//
+//  Created by Atheer Alahmari on 09/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
